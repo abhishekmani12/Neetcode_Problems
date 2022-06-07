db204c21c431e1e9fa1ef95885fdfcfb122ff292
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2022-06-07 16:31:58 +0530

    Update Valid_Sudoku.java

--- Arrays_&_Hashing/Valid_Sudoku.java
+++ Arrays_&_Hashing/Valid_Sudoku.java
@@ -1,28 +1,41 @@
+//SLOW SOLUTION:
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> h1 = new HashSet<String>();
         
-        for(int i=0; i < 9; i++){
-            for(int j=0; j< 9; j++){
-                if(board[i][j] != '.'){
+        //HashSet for row, column and 3X3 squares(Total: 9)
+        
+        HashSet<String> hset=new HashSet<String>();
+        
+        //Iterating through rows and columns
+        
+        for(int r=0;r<9;r++)
+        {
+            for(int c=0;c<9;c++)
+            {
+                if(board[r][c] !='.') // (i) Checking if there is an empty element(which is denoted as ' . ')
+                {
                     
-                //Check whether HashSet contains duplicate elements in row and column 
-                if(h1.contains("row" + i + board[i][j]) || h1.contains("col" + j + board[i][j]) ){
-                    return false;
-                }
-                h1.add("row" + i + board[i][j]);
-                h1.add("col" + j + board[i][j]);
-                
-                
-                //Check whether Box contains duplicate elements in it
-                if(h1.contains("box"+ (i/3)*3 + j/3 + board[i][j])){
-                    return false;
-                }
-                h1.add("box"+ (i/3)*3 + j/3 + board[i][j]);
+                   if(hset.contains("Row"+r+board[r][c]) || hset.contains("Column"+c+board[r][c])) // (ii) Checking for duplicates in Row and Column
+                   {
+                       return false;
+                   }
+                    //Updating Row and Column
+                    
+                    hset.add("Row"+r+board[r][c]);
+                    hset.add("Column"+c+board[r][c]);
+                    
+                    
+                    if(hset.contains("squares"+(r/3)*3+c/3+board[r][c]))// (iii) Checking for duplicates in each of the 3X3 squares---key: [r/3][c/3]
+                    {
+                        return false;
+                    }
+                    //updating square
+                    
+                    hset.add("squares"+(r/3)*3+c/3+board[r][c]);
                 }
             }
         }
-        
         
         return true;
     }
