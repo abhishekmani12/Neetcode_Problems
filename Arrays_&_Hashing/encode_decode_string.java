660cbcccb2103d1db787f8fc9cb845ba9fc2a044
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2022-07-21 22:47:14 +0530

    Update and rename encode.java to encode_decode_string.java

--- Arrays_&_Hashing/encode_decode_string.java
+++ Arrays_&_Hashing/encode_decode_string.java
@@ -1,23 +1,35 @@
 public class Solution {
 
     public String encode(List<String> strs) {
+        
         StringBuilder encodedString = new StringBuilder();
-        for (String str : strs) {
+        
+        for (String str : strs)
+        {
             encodedString.append(str.length()).append("#").append(str);
         }
+        
         return encodedString.toString();
     }
 
-    public List<String> decode(String str) {
+    public List<String> decode(String str) 
+    {
+        List<String> list = new ArrayList<>();
         
-        while (i < str.length()) {
+        int i = 0;
+        while (i < str.length()) 
+        {
             int j = i;
+            
             while (str.charAt(j) != '#') j++;
 
             int length = Integer.valueOf(str.substring(i, j));
+            
             i = j + 1 + length;
+            
             list.add(str.substring(j + 1, i));
         }
+        
         return list;
     }
 }
