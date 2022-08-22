f8d73aaeb5a771dae5e10f70657a5aa1acdbc32d
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2022-08-22 17:23:57 +0530

    Create ReverseLinkedList(iterative).java

---
+++ LinkedList/ReverseLinkedList(iterative).java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode p = null;
+        ListNode q = null;
+        ListNode r = head;
+        while (r != null) {
+            p = q;
+            q = r;
+            r = r.next;
+            q.next = p;
+        }
+        return q;
+    }
+}
